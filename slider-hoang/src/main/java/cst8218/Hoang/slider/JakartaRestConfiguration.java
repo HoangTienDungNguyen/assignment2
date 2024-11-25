082d87e777550cce17ad6539107cc437e30df746
RuochenLiuHarry <14575harry@gmail.com>
2024-11-25 18:34:56 -0500

    account administration

--- slider-hoang/src/main/java/cst8218/Hoang/slider/JakartaRestConfiguration.java
+++ slider-hoang/src/main/java/cst8218/Hoang/slider/JakartaRestConfiguration.java
@@ -1,11 +1,29 @@
 package cst8218.Hoang.slider;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
+ * @author RuochenLiu
  */
-@ApplicationPath("/resources")
+
+@BasicAuthenticationMechanismDefinition
+@DatabaseIdentityStoreDefinition(
+    dataSourceLookup = "java:comp/DefaultDataSource",
+    callerQuery = "SELECT password FROM app.appuser WHERE userid = ?",
+    groupsQuery = "SELECT groupname FROM app.appuser WHERE userid = ?",
+    hashAlgorithm = Pbkdf2PasswordHash.class,
+    priority = 10
+)
+@Named
+@ApplicationScoped
+@ApplicationPath("resources")
 public class JakartaRestConfiguration extends Application {
+    
 }
